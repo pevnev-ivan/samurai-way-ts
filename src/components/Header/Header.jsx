4b803eec860f6d44008b9a2b9a5a0ab20538dcaf
pevnev-ivan <pevnev.ivan.a@gmail.com>
2022-07-07 03:25:27 +0700

    Added CSS modules

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css'
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='logo.jpg' alt='logo'/>
+    </header>
+}
+
+export default Header
